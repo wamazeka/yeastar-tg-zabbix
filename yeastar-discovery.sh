df27c94fc7ae7e8cb65bf0df17c90ebafe906cf0
wamazeka <wamazeka@ya.ru>
2023-03-15 12:01:37 +0400

    Rework balance check
    Move balance check to SH script from template, add some description

--- yeastar-discovery.sh
+++ yeastar-discovery.sh
@@ -5,8 +5,23 @@
 #
 # Parameters: $discovery_2_3_etc $IP $API.USER $API.PASS
 
-# Discovery information about installed SIM-cards
-# key - discovery
+# discovery -- Discovery information about installed SIM-cards
+# scriptname.sh discovery IP_address APIUSER APIPASS
+
+# balance -- get number balance
+# scriptname.sh balance IP_address APIUSER APIPASS SPAN_NUM TIMEOUT_DEFAULT
+
+# else -- get info about certain span
+# scriptname.sh SPAN_NUM
+
+declare -a USSD
+USSD["79037011111"]="*102#" # Beeline
+USSD["79262909090"]="*100#" # Megafon
+USSD["79219909090"]="*100#" # Megafon_2
+USSD["79202909090"]="*100#" # Megafon_3
+USSD["79043490000"]="*105#" # Tele2
+USSD["79168999100"]="*100#" # MTS
+USSD["79112009993"]="*100#" # MTS_2
 
 IFS=$'\n'
 JSON="{\"data\":["
@@ -14,6 +29,7 @@ SEP=""
 
 if [[ $1 = "discovery" ]]; then
 	get=$(echo -e "Action: Login\nUsername: $3\nSecret: $4\n\nAction: smscommand\nCommand: gsm show spans\n\nAction: Logoff\n\n" | nc $2 5038 | grep "span [0-9]*:")
+
 	for pool in $get; do
 		id=$(echo $pool | sed "s/.* span //g" | sed "s/:.*//g")
 		# Examples:
@@ -31,11 +47,30 @@ if [[ $1 = "discovery" ]]; then
 	done
 	JSON=$JSON"]}"
 	echo $JSON
+fi
 
-# Get information about certain SIM-card
-# key - from discovery ("2", "3" etc.)
+if [[ $1 = "balance" ]]; then
+	smscenter=$(echo -e "Action: Login\nUsername: $3\nSecret: $4\n\nAction: smscommand\nCommand: gsm show span $5\n\nAction: Logoff\n\n" | nc $2 5038 | grep 'SMS Center' | tr -d [:blank:] | tr -d [:alpha:] | tr -d :+)
+
+	ussdresponce=$(echo -e "Action: Login\nUsername: $3\nSecret: $4\n\nAction: smscommand\nCommand: gsm send ussd $5 ${USSD["$smscenter"]} $7\n\nAction: Logoff\n\n" | nc $2 5038 | grep "USSD Message")
+
+	# Uppercase for minus catching, delete all spaces, change comma to dot
+	ussdresponce=$(echo $ussdresponce | tr [:lower:] [:upper:] | tr -d [:blank:] | tr , . | tr -d :)
+
+	# catch minus
+	ussdresponce=${ussdresponce//'MINUS'/'-'}
+	ussdresponce=${ussdresponce//'МИНУС'/'-'}
+	ussdresponce=${ussdresponce//'Р.'/''}
+	ussdresponce=${ussdresponce//'P.'/''}
+
+	# delete all spaces, all letters
+	ussdresponce=$(echo $ussdresponce | tr -d [:blank:] | tr -cd "1234567890.-")
+
+	echo $ussdresponce
 
 else
+	# Get information about certain SIM-card
+	# key - from discovery ("2", "3" etc.)
 	echo -e "Action: Login\nUsername: $3\nSecret: $4\n\nAction: smscommand\nCommand: gsm show span $1\n\nAction: Logoff\n\n" | nc $2 5038
 
 fi
